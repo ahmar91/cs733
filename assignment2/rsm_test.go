7e3459ce8cd87b56ba821a413f796ad776296c9b
Ahmar <mdahmar@cse.iitb.ac.in>
2016-02-21 16:44:50 +0530

    Test Cases Added

--- assignment2/rsm_test.go
+++ assignment2/rsm_test.go
@@ -3,30 +3,110 @@ package main
 import (
 	"testing"
 	"fmt"
+	"strconv"
 	//"math/rand"
 )
 
-func TestAppendEntriesRPC(t *testing.T){
+func TestAppend(t *testing.T){
 	var sm StateMachine
-	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfid:34,currState:"follower"}
+	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfid:34,leaderId:34,currState:"leader"}
 	sm.clusterSize = len(sm.serverIds)+1
+	sm.log = make([]logEntry,5)
+	for i := 0;i<len(sm.log);i++{
+		sm.log[i].term = i+1
+		sm.log[i].command = []byte{'j','m','p'}
+	}
+	sm.nextIndex = make(map[int]int)
+	for i := 0;i<len(sm.serverIds);i++{
+		sm.nextIndex[sm.serverIds[i]] = 1
+	}
+//	fmt.Println("hello")
+	z := sm.ProcessEvent(Append{data:[]byte{'m','o','v'}})
+//	fmt.Println("world")
+	var numSend int
+	var numLogStore int
+	var incorrectAction int
+	for i := 0; i<len(z);i++ {
+		switch z[i].(type){
+		case Send:
+			numSend++
+		case LogStore:
+			numLogStore++
+		default:
+			incorrectAction++
+		}
+	}
+	// fmt.Println("num of sends: " + strconv.Itoa(numSend))
+	// fmt.Println("num of log stores " + strconv.Itoa(numLogStore))
+	// fmt.Println("num of incorrect actions " + strconv.Itoa(incorrectAction))
+//	x := strconv.Itoa(numSend)
+	expect(t,strconv.Itoa(numSend),strconv.Itoa(len(sm.serverIds)))
+	expect(t,strconv.Itoa(numLogStore),"1")
+	expect(t,strconv.Itoa(incorrectAction),"0")
+}
+
+func TestAppendEntriesReq(t *testing.T){
+	var sm StateMachine
+	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfid:34,timer:1000,currState:"follower"}
+	sm.clusterSize = len(sm.serverIds)+1
+	sm.log = make([]logEntry,10)
+	for i := 0;i<len(sm.log);i++{
+		sm.log[i].term = i+1
+		sm.log[i].command = []byte{'j','m','p'}
+	}
+	sm.nextIndex = make(map[int]int)
+	for i := 0;i<len(sm.serverIds);i++{
+		sm.nextIndex[sm.serverIds[i]] = 1
+	}	
 	var loc_entries []logEntry
 	loc_entries = []logEntry{
-			{term:1,command:[]byte{1,2,3}},
-			{term:2,command:[]byte{4,5,6}},
+			{term:1,command:[]byte{'c','m','p'}},
+			{term:2,command:[]byte{'a','d','d'}},
 			}
 	z := sm.ProcessEvent(
 	AppendEntriesReq{
-	senderTerm : 1, 
+	senderTerm : 3, 
 	senderId: 5, 
-	prevLogIndex: 100, 
-	prevLogTerm: 3, 
+	prevLogIndex: 3, 
+	prevLogTerm: 4, 
 	entries: loc_entries,
 	senderCommitIndex: 1})
-	f,ok := z[0].(Send)
-	if ok{
-	fmt.Printf("%v\n", f)
-	}		
+	var numSend int
+	var numLogStore int
+	var numAlarm int
+	var numStateStore int
+	var numCommit int
+	var incorrectAction int
+	for i := 0; i<len(z);i++ {
+		switch z[i].(type){
+		case Send:
+			numSend++
+		case LogStore:
+			numLogStore++
+		case Alarm:
+			numAlarm++
+		case StateStore:
+			numStateStore++
+		case Commit:
+			numCommit++
+		default:
+			incorrectAction++
+		}
+	}
+	// fmt.Println("num of sends: " + strconv.Itoa(numSend))
+	// fmt.Println("num of log stores " + strconv.Itoa(numLogStore))
+	// fmt.Println("num of alarms " + strconv.Itoa(numAlarm))
+	// fmt.Println("num of state stores " + strconv.Itoa(numStateStore))
+	// fmt.Println("num of commits " + strconv.Itoa(numCommit))
+	// fmt.Println("num of incorrect actions " + strconv.Itoa(incorrectAction))
+	expect(t,strconv.Itoa(numSend),"1")
+	expect(t,strconv.Itoa(numLogStore),"2")
+	expect(t,strconv.Itoa(numAlarm),"1")
+	expect(t,strconv.Itoa(numStateStore),"1")
+	expect(t,strconv.Itoa(numCommit),"1")
+	expect(t,strconv.Itoa(incorrectAction),"0")		
+}
+
 /*	sm.currState = 3
 	a:=sm.ProcessEvent(VoteReqEv{candidateId:10,term:20,lastLogIndex:30,lastLogTerm:5})
 	
@@ -43,7 +123,6 @@ func TestAppendEntriesRPC(t *testing.T){
 	sm.state = 3
 	a:=sm.ProcessEvent(VoteRespEv{senderTerm: 3, response:true})
 	
-*/
 	/*sm.state=3
 	z :=[]byte{1,2,3,4}
 	a:= sm.ProcessEvent(Append{data:z})
@@ -62,14 +141,17 @@ func TestAppendEntriesRPC(t *testing.T){
 	f,ok := a[0].(Alarm)
 	if ok {
 	fmt.Printf("%v\n", f)	
-	}*/
+	}
+*/
 	
-
-
-
-}
 /*
 func TestAppendFollower(t *testing.T){
 	var sm StateMachine
 
 }*/
+// Useful testing function
+func expect(t *testing.T, a string, b string) {
+        if a != b {
+                t.Error(fmt.Sprintf("Expected %v, found %v", b, a)) // t.Error is visible when running `go test -verbose`
+        }
+}
