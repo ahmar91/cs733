06ed707286d48a067f398a19a7ced348d28f165e
Ahmar <mdahmar@cse.iitb.ac.in>
2016-05-01 04:54:24 +0530

    StateMachine updated final

--- assignment2/rsm_test.go
+++ assignment2/rsm_test.go
@@ -8,18 +8,18 @@ import (
 
 func TestAppend(t *testing.T){
 	var sm StateMachine
-	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfid:34,leaderId:34,currState:"leader"}
+	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfId:34,leaderId:34,currState:"leader"}
 	sm.clusterSize = len(sm.serverIds)+1
-	sm.log = make([]logEntry,5)
+	sm.log = make([]LogEntry,5)
 	for i := 0;i<len(sm.log);i++{
-		sm.log[i].term = i+1
-		sm.log[i].command = []byte{'j','m','p'}
+		sm.log[i].Term = i+1
+		sm.log[i].Command = []byte{'j','m','p'}
 	}
-	sm.nextIndex = make(map[int]int)
+	sm.nextIndex = make(map[int]int64)
 	for i := 0;i<len(sm.serverIds);i++{
 		sm.nextIndex[sm.serverIds[i]] = 1
 	}
-	z := sm.ProcessEvent(Append{data:[]byte{'m','o','v'}})
+	z := sm.ProcessEvent(Append{Data:[]byte{'m','o','v'}})
 	var numSend int
 	var numLogStore int
 	var incorrectAction int
@@ -40,16 +40,16 @@ func TestAppend(t *testing.T){
 
 func TestTimeout(t *testing.T){
 	var sm StateMachine
-	sm = StateMachine{currTerm: 4,serverIds: []int{5,13,21,55},selfid:34,timer:1000,currState:"leader"}
+	sm = StateMachine{currTerm: 4,serverIds: []int{5,13,21,55},selfId:34,timer:1000,currState:"leader"}
 	sm.clusterSize = len(sm.serverIds)+1
-	sm.log = make([]logEntry,20)
+	sm.log = make([]LogEntry,20)
 	for i := 0;i<len(sm.log);i++{
-		sm.log[i].term = i+1
-		sm.log[i].command = []byte{'j','m','p'}
+		sm.log[i].Term = i+1
+		sm.log[i].Command = []byte{'j','m','p'}
 	}
 	sm.lastLogIndex = 19
-	sm.nextIndex = make(map[int]int)
-	sm.matchIndex = make(map[int]int)
+	sm.nextIndex = make(map[int]int64)
+	sm.matchIndex = make(map[int]int64)
 	for i := 0;i<len(sm.serverIds);i++{
 		sm.nextIndex[sm.serverIds[i]] = 1
 		sm.matchIndex[sm.serverIds[i]] = 0
@@ -65,7 +65,7 @@ func TestTimeout(t *testing.T){
 		switch z[i].(type){
 		case Send:
 			a,_ := z[i].(Send)
-			_,ok := a.event.(AppendEntriesReq)
+			_,ok := a.Event.(AppendEntriesReq)
 			if ok{
 				numSend++
 			}else{
@@ -85,7 +85,7 @@ func TestTimeout(t *testing.T){
 	}
 	expect(t,strconv.Itoa(numSend),"4")
 	expect(t,strconv.Itoa(numLogStore),"0")
-	expect(t,strconv.Itoa(numAlarm),"1")
+	expect(t,strconv.Itoa(numAlarm),"0")
 	expect(t,strconv.Itoa(numStateStore),"0")
 	expect(t,strconv.Itoa(numCommit),"0")
 	expect(t,strconv.Itoa(incorrectAction),"0")
@@ -93,30 +93,30 @@ func TestTimeout(t *testing.T){
 
 func TestAppendEntriesReq(t *testing.T){
 	var sm StateMachine
-	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfid:34,timer:1000,currState:"follower"}
+	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfId:34,timer:1000,currState:"follower",electionAlarmPeriod:5,heartbeatAlarmPeriod:4}
 	sm.clusterSize = len(sm.serverIds)+1
-	sm.log = make([]logEntry,10)
+	sm.log = make([]LogEntry,10)
 	for i := 0;i<len(sm.log);i++{
-		sm.log[i].term = i+1
-		sm.log[i].command = []byte{'j','m','p'}
+		sm.log[i].Term = i+1
+		sm.log[i].Command = []byte{'j','m','p'}
 	}
-	sm.nextIndex = make(map[int]int)
+	sm.nextIndex = make(map[int]int64)
 	for i := 0;i<len(sm.serverIds);i++{
 		sm.nextIndex[sm.serverIds[i]] = 1
 	}	
-	var loc_entries []logEntry
-	loc_entries = []logEntry{
-			{term:1,command:[]byte{'c','m','p'}},
-			{term:2,command:[]byte{'a','d','d'}},
+	var loc_entries []LogEntry
+	loc_entries = []LogEntry{
+			{Term:1,Command:[]byte{'c','m','p'}},
+			{Term:2,Command:[]byte{'a','d','d'}},
 			}
 	z := sm.ProcessEvent(
 	AppendEntriesReq{
-	senderTerm : 3, 
-	senderId: 5, 
-	prevLogIndex: 3, 
-	prevLogTerm: 4, 
-	entries: loc_entries,
-	senderCommitIndex: 1})
+	SenderTerm : 3, 
+	SenderId: 5, 
+	PrevLogIndex: 3, 
+	PrevLogTerm: 4, 
+	Entries: loc_entries,
+	SenderCommitIndex: 1})
 	var numSend int
 	var numLogStore int
 	var numAlarm int
@@ -127,7 +127,7 @@ func TestAppendEntriesReq(t *testing.T){
 		switch z[i].(type){
 		case Send:
 			a,_ := z[i].(Send)
-			_,ok := a.event.(AppendEntriesResp)
+			_,ok := a.Event.(AppendEntriesResp)
 			if ok{
 				numSend++
 			}else{
@@ -146,35 +146,35 @@ func TestAppendEntriesReq(t *testing.T){
 		}
 	}
 	expect(t,strconv.Itoa(numSend),"1")
-	expect(t,strconv.Itoa(numLogStore),"2")
+	expect(t,strconv.Itoa(numLogStore),"0")
 	expect(t,strconv.Itoa(numAlarm),"1")
 	expect(t,strconv.Itoa(numStateStore),"1")
-	expect(t,strconv.Itoa(numCommit),"1")
+	expect(t,strconv.Itoa(numCommit),"0")
 	expect(t,strconv.Itoa(incorrectAction),"0")		
 }
 
 func TestAppendEntriesResp(t *testing.T){
 	var sm StateMachine
-	sm = StateMachine{currTerm: 4,serverIds: []int{5,13,21,55},selfid:34,timer:1000,currState:"leader"}
+	sm = StateMachine{currTerm: 4,serverIds: []int{5,13,21,55},selfId:34,timer:1000,currState:"leader",electionAlarmPeriod:5,heartbeatAlarmPeriod:4}
 	sm.clusterSize = len(sm.serverIds)+1
-	sm.log = make([]logEntry,20)
+	sm.log = make([]LogEntry,20)
 	for i := 0;i<len(sm.log);i++{
-		sm.log[i].term = i+1
-		sm.log[i].command = []byte{'j','m','p'}
+		sm.log[i].Term = i+1
+		sm.log[i].Command = []byte{'j','m','p'}
 	}
 	sm.lastLogIndex = 19
-	sm.nextIndex = make(map[int]int)
-	sm.matchIndex = make(map[int]int)
+	sm.nextIndex = make(map[int]int64)
+	sm.matchIndex = make(map[int]int64)
 	for i := 0;i<len(sm.serverIds);i++{
 		sm.nextIndex[sm.serverIds[i]] = 1
 		sm.matchIndex[sm.serverIds[i]] = 0
 	}	
 	z := sm.ProcessEvent(
 	AppendEntriesResp{
-	senderTerm : 3, 
-	senderId: 5, 
-	lastMatchInd: 2,
-	response: false})
+	SenderTerm : 3, 
+	SenderId: 5, 
+	SenderLastRepIndex: 2,
+	Response: false})
 	var numSend int
 	var numLogStore int
 	var numAlarm int
@@ -185,7 +185,7 @@ func TestAppendEntriesResp(t *testing.T){
 		switch z[i].(type){
 		case Send:
 			a,_ := z[i].(Send)
-			_,ok := a.event.(AppendEntriesReq)
+			_,ok := a.Event.(AppendEntriesReq)
 			if ok{
 				numSend++
 			}else{
@@ -213,23 +213,23 @@ func TestAppendEntriesResp(t *testing.T){
 
 func TestVoteReq(t *testing.T){
 	var sm StateMachine
-	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfid:34,timer:1000,currState:"follower"}
+	sm = StateMachine{currTerm: 2,serverIds: []int{5,13,21,55},selfId:34,timer:1000,currState:"follower",electionAlarmPeriod:5,heartbeatAlarmPeriod:4}
 	sm.clusterSize = len(sm.serverIds)+1
-	sm.log = make([]logEntry,20)
+	sm.log = make([]LogEntry,20)
 	for i := 0;i<len(sm.log);i++{
-		sm.log[i].term = i+1
-		sm.log[i].command = []byte{'j','m','p'}
+		sm.log[i].Term = i+1
+		sm.log[i].Command = []byte{'j','m','p'}
 	}
 	sm.lastLogIndex = 19
 	sm.lastLogTerm = 4
 	sm.votedFor = -1
-	sm.nextIndex = make(map[int]int)
-	sm.matchIndex = make(map[int]int)
+	sm.nextIndex = make(map[int]int64)
+	sm.matchIndex = make(map[int]int64)
 	for i := 0;i<len(sm.serverIds);i++{
 		sm.nextIndex[sm.serverIds[i]] = 1
 		sm.matchIndex[sm.serverIds[i]] = 0
 	}	
-	z := sm.ProcessEvent(VoteReq{senderId:21,senderTerm:3,senderLastLogIndex:13,senderLastLogTerm:5})
+	z := sm.ProcessEvent(VoteReq{SenderId:21,SenderTerm:3,SenderLastLogIndex:13,SenderLastLogTerm:5})
 	var numSend int
 	var numLogStore int
 	var numAlarm int
@@ -240,7 +240,7 @@ func TestVoteReq(t *testing.T){
 		switch z[i].(type){
 		case Send:
 			a,_ := z[i].(Send)
-			_,ok := a.event.(VoteResp)
+			_,ok := a.Event.(VoteResp)
 			if ok{
 				numSend++
 			}else{
@@ -261,29 +261,29 @@ func TestVoteReq(t *testing.T){
 	expect(t,strconv.Itoa(numSend),"1")
 	expect(t,strconv.Itoa(numLogStore),"0")
 	expect(t,strconv.Itoa(numAlarm),"0")
-	expect(t,strconv.Itoa(numStateStore),"1")
+	expect(t,strconv.Itoa(numStateStore),"2")
 	expect(t,strconv.Itoa(numCommit),"0")
 	expect(t,strconv.Itoa(incorrectAction),"0")	
 }
 
 func TestVoteResp(t *testing.T){
 	var sm StateMachine
-	sm = StateMachine{currTerm: 3,serverIds: []int{5,13},selfid:34,timer:1000,currState:"follower"}
+	sm = StateMachine{currTerm: 3,serverIds: []int{5,13},selfId:34,timer:1000,currState:"follower",electionAlarmPeriod:5,heartbeatAlarmPeriod:4}
 	sm.clusterSize = len(sm.serverIds)+1
-	sm.log = make([]logEntry,20)
+	sm.log = make([]LogEntry,20)
 	for i := 0;i<len(sm.log);i++{
-		sm.log[i].term = i+1
-		sm.log[i].command = []byte{'j','m','p'}
+		sm.log[i].Term = i+1
+		sm.log[i].Command = []byte{'j','m','p'}
 	}
 	sm.lastLogIndex = 19
-	sm.nextIndex = make(map[int]int)
-	sm.matchIndex = make(map[int]int)
+	sm.nextIndex = make(map[int]int64)
+	sm.matchIndex = make(map[int]int64)
 	for i := 0;i<len(sm.serverIds);i++{
 		sm.nextIndex[sm.serverIds[i]] = 1
 		sm.matchIndex[sm.serverIds[i]] = 0
 	}
 	x := sm.ProcessEvent(Timeout{})	
-	y := sm.ProcessEvent(VoteResp{senderId:5,senderTerm:4,response:true})
+	y := sm.ProcessEvent(VoteResp{SenderId:5,SenderTerm:4,Response:true})
 	z := append(x,y...)
 	var numSendAppendEntriesReq int
 	var numSendVoteReq int
@@ -296,11 +296,11 @@ func TestVoteResp(t *testing.T){
 		switch z[i].(type){
 		case Send:
 			a,_ := z[i].(Send)
-			_,ok1 := a.event.(AppendEntriesReq)
+			_,ok1 := a.Event.(AppendEntriesReq)
 			if ok1{
 				numSendAppendEntriesReq++
 			}else{
-				_,ok2 := a.event.(VoteReq)
+				_,ok2 := a.Event.(VoteReq)
 				if ok2{
 					numSendVoteReq++
 				}else{	
@@ -322,7 +322,7 @@ func TestVoteResp(t *testing.T){
 	expect(t,strconv.Itoa(numSendAppendEntriesReq),"2")
 	expect(t,strconv.Itoa(numSendVoteReq),"2")
 	expect(t,strconv.Itoa(numLogStore),"0")
-	expect(t,strconv.Itoa(numAlarm),"1")
+	expect(t,strconv.Itoa(numAlarm),"2")
 	expect(t,strconv.Itoa(numStateStore),"1")
 	expect(t,strconv.Itoa(numCommit),"0")
 	expect(t,strconv.Itoa(incorrectAction),"0")
